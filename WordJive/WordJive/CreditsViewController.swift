88b2210b2f5ed1fd73eff3768a8e42c209e164f6
Donny Davis <donnywdavis@icloud.com>
2016-05-19 16:46:50 -0400

    Even more UI tweaks.
    It’s looking good!

--- WordJive/WordJive/CreditsViewController.swift
+++ WordJive/WordJive/CreditsViewController.swift
@@ -10,8 +10,55 @@ import UIKit
 
 class CreditsViewController: UIViewController {
 
+    @IBOutlet weak var iOSDevelopersLabel: UILabel!
+    @IBOutlet weak var allenLabel: UILabel!
+    @IBOutlet weak var oliverLabel: UILabel!
+    @IBOutlet weak var donnyLabel: UILabel!
+    @IBOutlet weak var nickLabel: UILabel!
+    @IBOutlet weak var javaDevelopersLabel: UILabel!
+    @IBOutlet weak var daneLabel: UILabel!
+    @IBOutlet weak var scottLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        iOSDevelopersLabel.layer.masksToBounds = true
+        iOSDevelopersLabel.layer.cornerRadius = 10
+        javaDevelopersLabel.layer.masksToBounds = true
+        javaDevelopersLabel.layer.cornerRadius = 10
+        
+        UIView.animateWithDuration(0.5) {
+            self.allenLabel.center.x += self.view.bounds.width
+        }
+        
+        UIView.animateWithDuration(0.5, delay: 0.3, options: [.CurveEaseOut], animations: {
+            self.oliverLabel.center.x += self.view.bounds.width
+            }, completion: nil)
+        
+        UIView.animateWithDuration(0.5, delay: 0.4, options: [.CurveEaseOut], animations: {
+            self.donnyLabel.center.x += self.view.bounds.width
+            }, completion: nil)
+        
+        UIView.animateWithDuration(0.5, delay: 0.5, options: [.CurveEaseOut], animations: {
+            self.nickLabel.center.x += self.view.bounds.width
+            }, completion: nil)
+        
+        UIView.animateWithDuration(0.5, delay: 0.6, options: [.CurveEaseOut], animations: {
+            self.daneLabel.center.x += self.view.bounds.width
+            }, completion: nil)
+        
+        UIView.animateWithDuration(0.5, delay: 0.7, options: [.CurveEaseOut], animations: {
+            self.scottLabel.center.x += self.view.bounds.width
+            }, completion: nil)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        allenLabel.center.x -= view.bounds.width
+        oliverLabel.center.x -= view.bounds.width
+        donnyLabel.center.x -= view.bounds.width
+        nickLabel.center.x -= view.bounds.width
+        daneLabel.center.x -= view.bounds.width
+        scottLabel.center.x -= view.bounds.width
     }
 
     override func didReceiveMemoryWarning() {
