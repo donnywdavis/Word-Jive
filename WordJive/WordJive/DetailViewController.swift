2650e673927359a4017aced7a33c239a0bcf9718
Allen Spicer <allenspicer@gmail.com>
2016-05-19 15:07:03 -0400

    Initial Commit

--- WordJive/WordJive/DetailViewController.swift
+++ WordJive/WordJive/DetailViewController.swift
@@ -13,7 +13,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
 
-    var detailItem: AnyObject? {
+    var gameItem: AnyObject? {
         didSet {
             // Update the view.
             self.configureView()
@@ -22,17 +22,80 @@ class DetailViewController: UIViewController {
 
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = self.detailItem {
-            navigationController?.navigationItem.title = detail.valueForKey("title") as? String
+        if let game = self.gameItem {
+            navigationController?.navigationItem.title = game.valueForKey("title") as? String
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.configureView()
-    }
 
+   
+        var width = 2
+        var height = 2
+        
+            
+            //if something is input by user
+            if ((0<width) && (0<height) &&
+                (width<10) && (height<10)){
+                
+                //change user input to int
+                var x = Int(width)
+                var y = Int(height)
+                
+                //store x value for later
+                let xSub = x
+                
+                //loop through y values (columns)
+                while (y>0){
+                    
+                    //loop through x values (rows). One row for each y value
+                    while (x>0) {
+                        
+                        //each time through fire label creator
+                        labelCreator(x,b:y)
+                        x = x-1
+                    }
+                    x = xSub
+                    y = y-1
+                }
+                
+            }
+            
+    
+    
+    self.configureView()
+}
+
+        func labelCreator(a:Int, b:Int){
+            //convert int to float
+            let a10 = CGFloat(a*35)
+            let b10 = CGFloat(b*35)
+            
+            //create unique frame on the view using these inputs
+            let labelPlacement = CGRectMake(a10, b10, CGFloat(30), CGFloat(30))
+            
+            //create a label with the frame
+            let newLabel = UILabel.init(frame: labelPlacement)
+            
+            //substitute this text for specific index of array and loop through.
+            newLabel.text = "A"
+            newLabel.userInteractionEnabled = true
+            self.view.insertSubview(newLabel, atIndex: a)
+        }
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
